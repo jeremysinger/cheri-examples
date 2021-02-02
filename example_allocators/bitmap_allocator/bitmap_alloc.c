f0c4e47bb2418318504cc23d54ba04b229738c6a
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2021-02-02 15:59:16 +0000

    tidying up bitmap allocator

--- example_allocators/bitmap_allocator/bitmap_alloc.c
+++ example_allocators/bitmap_allocator/bitmap_alloc.c
@@ -17,6 +17,11 @@
  * first free chunk, which is expensive!
  * More efficient scans could be easily incorporated.
  *
+ * This is _not_ a general-purpose allocator, since
+ * it only allocates chunks of a fixed size. Further,
+ * this size is constrained to be small enough to allow
+ * exact bounds representation in CHERI capabilities.
+ *
  * This is an initial simple memory allocator test
  * for CHERI / Capable VMs.
  * We explore capability alignment, 
@@ -36,7 +41,7 @@
 #define BITS_PER_BYTE 8
 
 char *buffer = NULL; /* allocation buffer */
-char *bitmap = NULL;  /* bitmap for the buffer */
+unsigned char *bitmap = NULL;  /* bitmap for the buffer */
 
 int buffer_size = 0;  /* size of buffer (in bytes) */
 int bitmap_size = 0;   /* size of bitmap (in bytes) */
@@ -46,27 +51,33 @@ void init_alloc(int num_chunks, int chunk_size)
 {
         int i = 0;
 
-	/* do we need to increase the num_chunks
-	 * so every bit in bitmap will be used?
+	/* we need to increase the num_chunks
+	 * so every bit in bitmap will be used
 	 */
 	int adjusted_num_chunks =
 	  (num_chunks%BITS_PER_BYTE==0)?
 	  num_chunks:
 	  (num_chunks + (BITS_PER_BYTE-(num_chunks%BITS_PER_BYTE)));
 	
-	/* do we need to increase the chunk_size
-	 * so chunks will be CHERI aligned?
-	 * (i.e. 16 bytes for a 64-bit architecture)
+	/* we need to increase the chunk_size
+	 * so chunks will be CHERI aligned
+	 * (i.e. 16 bytes for RISC-V 64-bit arch)
 	 */
 	int adjusted_chunk_size =
 	  (chunk_size%(sizeof(void *)) == 0)?
 	  chunk_size:
 	  (chunk_size + (sizeof(void *))-(chunk_size%(sizeof(void *))));
 
+	/* check this chunk size is small enough 
+	 * so we can represent bounds precisely with
+	 * CHERI compressed representation
+	 */
+	// FIXME
+
 	/* request memory for our allocation buffer */
 	char *res = mmap(NULL, adjusted_num_chunks*adjusted_chunk_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 	/* request memory for our bitmap */
-	bitmap = mmap(NULL, adjusted_num_chunks/BITS_PER_BYTE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	bitmap = (unsigned char*)mmap(NULL, adjusted_num_chunks/BITS_PER_BYTE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
 	if (res == MAP_FAILED || bitmap == MAP_FAILED)
 	{
@@ -103,7 +114,7 @@ void init_alloc(int num_chunks, int chunk_size)
  */
 char *alloc_chunk()
 {
-        char updated_byte = 0;
+        unsigned char updated_byte = 0;
 	int chunk_index = 0;
 	char *chunk = NULL;
 	// iterate over all bits in bitmap, looking for a 0
@@ -111,15 +122,13 @@ char *alloc_chunk()
 	// return the corresponding chunk
 	// (setting its capability bounds)
 	int i = 0;
-	// printf("alloc_chunk()\n");
-	while (bitmap[i] == (char)0xff) {
+	while (bitmap[i] == (unsigned char)0xff) {
 	  i++;
 	  if (i>=bitmap_size)
 	    break;
 	}
 	// do we have a 0?
-	if (i < bitmap_size && bitmap[i] != (char)0xff) {
-	  // printf("looking for j bit set to 1...\n");
+	if (i < bitmap_size && bitmap[i] != (unsigned char)0xff) {
 	  // find the lowest 0 ...
 	  int j = 0;
 	  // right shift until bottom bit is 0
@@ -129,36 +138,33 @@ char *alloc_chunk()
 	      break;
 	    }
 	  }
-	  // printf("j is %d\n", j);
-	  // ok - so now i is the word, j is the bit
+	  // now i is the word index, j is the bit index
 	  // set this bit to 1 ...
 	  // and work out the chunk to allocate
-	  updated_byte = bitmap[i] + (char)(1<<j);
+	  updated_byte = bitmap[i] + (unsigned char)(1<<j);
 	  bitmap[i] = updated_byte;
-	  // printf("new bitmap entry %d at %d", bitmap[i], i);
+
 	  chunk_index = i*BITS_PER_BYTE + j;
 	  chunk = buffer + (chunk_index * bytes_per_chunk);
 	  /* restrict capability range before returning ptr */
 	  /// chunk = cheri_bounds_set_exact(chunk, BYTES_PER_CHUNK);
 	  // FIXME - what if this is not representable?
 	}
-	//else {
- 	//   printf("no room to alloc: i is %d\n bitmap_size is %d, bitmap[i] & 0xff is %d\n", i, bitmap_size, bitmap[i]&0xff);
-	//}
 
 	// CHERI set bounds for chunk
 	
 	return chunk;
 }
 
+
 void free_chunk(char *chunk) {
-  // work out chunk index in buffer
+  /* calculate chunk index in buffer */
   int chunk_index = (chunk - buffer) / bytes_per_chunk;
-  // work out equivalent bitmap index
+  /* calculate corresponding bitmap index */
   int bitmap_index = chunk_index / BITS_PER_BYTE;
   int bitmap_offset = chunk_index % BITS_PER_BYTE;
-  // set this bitmap value to 0
-  char updated_byte = bitmap[bitmap_index] & (char)(~(1<<bitmap_offset));
+  /* set this bitmap entry to 0 */
+  unsigned char updated_byte = bitmap[bitmap_index] & (unsigned char)(~(1<<bitmap_offset));
   bitmap[bitmap_index] = updated_byte;
   return;
 }
@@ -169,10 +175,10 @@ int num_used_chunks() {
   int used_chunks = 0;
   
   while (i<bitmap_size) {
-    char x = bitmap[i];
+    unsigned char x = bitmap[i];
     if (x != 0) {
       /* some used chunks here */
-      char j;
+      unsigned char j;
       for (j=1; j<=x; j=j<<1) {
 	if (x&j) {
 	  used_chunks++;
