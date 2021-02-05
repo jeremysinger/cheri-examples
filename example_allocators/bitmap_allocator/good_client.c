254488ca03c08a7735ae23b7d53ab1b3527c0e44
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2021-02-05 10:30:06 +0000

    nice linked list example for bitmap allocation

--- example_allocators/bitmap_allocator/good_client.c
+++ example_allocators/bitmap_allocator/good_client.c
@@ -15,40 +15,52 @@
 /* should program generate debugging output? 1 for Yes, 0 for No */
 #define DEBUG_PRINTF 1
 
-/* size of buffer to allocate (in words) */
-#define NUM_WORDS 64
+/* size of buffer to allocate (in list cells) */
+#define NUM_CELLS 64
+
+/* trivial linked list data structure */
+typedef struct _list {
+  int payload;
+  struct _list *next;;
+} list;
 
 int main()
 {
-	int i;
-	int *arr;
+        unsigned int i = 0;
+	unsigned int n = 1;
+	list *l = NULL;
 	
-	init_alloc(NUM_WORDS, sizeof(int));
+	init_alloc(NUM_CELLS, sizeof(list));
 
-	/* now try to do some bump pointer allocations */
-	for (i = 0; i < NUM_WORDS; i++)
-	{
-		int *x = (int *)alloc_chunk();
-		arr[i] = x;
-		if (DEBUG_PRINTF)
-		  //inspect_pointer(x);
-		if (x)
-		{
-			*x = 42;
-			if (DEBUG_PRINTF)
-				printf("stored value %d\n", *x);
+	while (n <= NUM_CELLS) {
+	
+	  /* now try to do some allocations */
+	  for (i = 0; i < n; i++)
+	  {
+		list *tmp = (int *)alloc_chunk();
+		if (tmp) {
+		  tmp->payload = i;
+		  tmp->next = l;
+		  printf("alloc cell %d\n", i);
+		  l = tmp;
 		}
-		else
-		{
-			if (DEBUG_PRINTF)
-				printf("bump_alloc returned null\n");
+		else {
+		  fprintf(stderr, "chunk allocation failed but there should be enough memory!");
+		  exit(-1);
 		}
-	}
+	  }
 
-	// now do some frees
-	for (i = 0; i < NUM_WORDS; i++)
-	{
-	  free_chunk(arr[i]);
+	  // now free this list
+	  while (l != NULL)
+	  {
+	    list *tmp = l->next;
+	    printf("free cell %d\n", l->payload);
+	    free_chunk(l);
+	    l = tmp;
+	  }
+
+	  /* double the limit, n */
+	  n <<= 1;
 	}
 	
 	return 0;
